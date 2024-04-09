7d5e16fcabff68f5383b17d4e83caac09f5505bc
bobbyhadz <opmftw@gmail.com>
2024-04-09 18:48:29 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Add elements to a List in a Loop in Python
+
+my_list = ['bobby', 'hadz', 'com']
+
+for i in range(3):
+    my_list.append('new')
+
+# 👇️ ['bobby', 'hadz', 'com', 'new', 'new', 'new']
+print(my_list)
